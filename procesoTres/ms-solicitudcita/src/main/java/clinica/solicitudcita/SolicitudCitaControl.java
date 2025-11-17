ad6e0eb8d5eabdffc8d5b4f71fe23ae7c9c5fc89
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-16 21:40:07 -0500

    fix: reorder imports and add CrossOrigin annotation for CORS support

--- procesoTres/ms-solicitudcita/src/main/java/clinica/solicitudcita/SolicitudCitaControl.java
+++ procesoTres/ms-solicitudcita/src/main/java/clinica/solicitudcita/SolicitudCitaControl.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 // Controlador REST para el proceso de solicitud de cita
 @RestController
 @RequestMapping("/solicitudcita")
